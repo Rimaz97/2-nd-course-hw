a0f6e88b130207cccd1906d4bf4e6308a76fbd8d
Rimaz <Rimaz_97@mail.email@example.com>
2024-12-29 01:11:51 +0300

    6 Домашка

--- hw-6.js
+++ hw-6.js
@@ -68,9 +68,98 @@ if (seven.includes(userInput)) {
 
 // Задача 8
 
-let eight = 'abcdef';
+let string = 'abcdef';
 
-console.log(eight.reverse()); // Выводим обратную строку
+// Преобразуем строку в массив символов
+let splitString = string.split("");
+
+// // Используем метод reverse() для изменения порядка элементов массива
+splitString.reverse();
+
+// // Конвертируем массив обратно в строку и выводим результат
+let joinedText = splitString.join("");
+console.log(joinedText); // Выводим результат в консоль
 
 // Задача 9
 
+let Nine = [[1, 2, 3], [4, 5, 6]];
+
+// Используем оператор spread для объединения элементов массивов
+let result = [...Nine];
+
+console.log(result); // Выводим результат в консоль
+
+// Задача 10
+
+let ten = [4, 2, 8, 3, 6, 1, 5, 9, 7]; // Пример массива с числами от 1 до 10
+
+for (let i = 0; i < ten.length; i++) {
+  if (i < ten.length - 1) { // Проверяем, есть ли следующий элемент
+    console.log(ten[i] + ten[i + 1]);
+  } else {
+    // Для последнего элемента выводим только текущий элемент
+    console.log(ten[i]);
+  }
+}
+
+// Задача 11
+
+function squareEleven(numbers) {
+  return numbers.map(number => number * number);
+}
+
+// Пример использования функции
+const inputEleven = [1, 2, 3, 4, 5];
+const outputEleven = squareEleven(inputEleven);
+
+console.log(outputEleven); // Выводим результат в консоль
+
+// Задача 12
+
+function twelve(words) {
+  return words.map(word => word.length);
+}
+
+// Пример использования функции
+const inputTwelve = ["apple", "banana", "cherry"];
+const outputTwelve = twelve(inputTwelve);
+
+console.log(outputTwelve); // Выводим результат в консоль
+
+// Задача 13
+
+function thirteen(numbers) {
+  return numbers.filter(number => number < 0);
+}
+
+// Пример использования функции
+const inputThirteen = [1, -2, 3, -4, 5];
+const outputThirteen = thirteen(inputThirteen);
+
+console.log(outputThirteen); // Выводим результат в консоль
+
+// Задача 14
+
+// Генерация исходного массива из 10 случайных чисел от 0 до 10
+const originalFourteen = Array.from({ length: 10 }, () => Math.floor(Math.random() * 11));
+
+// Создание нового массива с четными значениями
+const evenNumbersFourteen = originalFourteen.filter(number => number % 2 === 0);
+
+console.log("Исходный массив:", originalFourteen);
+console.log("Массив с четными числами:", evenNumbersFourteen);
+
+// Задача 15
+
+let fifteen = [];
+
+// Генерация массива из 6 случайных чисел от 1 до 10
+for (let i = 0; i < 6; i++) {
+  fifteen[i] = Math.floor(Math.random() * 10) + 1;
+}
+
+// Нахождение среднего арифметического
+const average = fifteen.reduce((a, b) => a + b, 0) / fifteen.length;
+
+// Вывод результата в консоль
+console.log("Среднее арифметическое:", average);
